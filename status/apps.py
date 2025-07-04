fbda18a267c647d2c81f7bb5ff4ebcd2633e1212
che-hyeon <ghkdcogus03@naver.com>
2025-07-05 05:55:16 +0900

    Feat: citizen 전체 필터 추가

--- status/apps.py
+++ status/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class StatusConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'status'
+
+    def ready(self):
+        import status.signals  # signals.py를 불러와 시그널 연결
